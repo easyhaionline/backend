9253dfbe100246b35b5bb31e653e8ad31273d925
niteshrawat0401 <niteshrawat0401gmail.com>
2023-03-03 11:13:54 +0530

    course management api

--- controllers/pushNotification.js
+++ controllers/pushNotification.js
@@ -1,18 +1,30 @@
-// const express = require("express");
-// const AWS = require('aws-sdk');
-// AWS.config.update({
-//     accessKeyId: process.env.Notification_Access_Key_Id,
-//     secretAccessKey: process.env.Notification_Secret_Access_Key,
-//     region: "us-east-1",
-//   });
+const express = require("express");
+const AWS = require('aws-sdk');
+AWS.config.update({
+    accessKeyId: process.env.Notification_Access_Key_Id,
+    secretAccessKey: process.env.Notification_Secret_Access_Key,
+    region: "us-east-1",
+  });
 
-// const pushNotification = async(request, response) => {
-// 	const { title, message, topicArn } = req.body;
+const pushNotification = async(request, response) => {
+	const { title, message, topicArn } = req.body;
 
+  var params = {
+    Message: JSON.stringify({title: req.body.title}), 
+    TopicArn:  "arn:aws:sns:us-east-1:896408422234:Demo_neet"
+  };
+
+  sns.publish(messageParams, (err, data) => {
+    if (err) {
+      console.error(`Error sending message: ${err.message}`);
+    } else {
+      console.log(`Message sent: ${data.MessageId}`);
+    }
+  });
 	
-// }
+}
 
 
-// module.exports = {
-//     pushNotification
-// }
+module.exports = {
+    pushNotification
+}
